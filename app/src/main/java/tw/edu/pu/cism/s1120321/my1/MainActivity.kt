4292ce8d86fffcf53563185a86c0d2940c21c0ae
s1120321 <yieewenhong@gmail.com>
2024-09-30 15:46:28 +0800

    加入樂透功能2

--- app/src/main/java/tw/edu/pu/cism/s1120321/my1/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/cism/s1120321/my1/MainActivity.kt
@@ -1,6 +1,7 @@
 package tw.edu.pu.cism.s1120321.my1
 
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,5 +20,10 @@ class MainActivity : AppCompatActivity() {
         }
         var txv: TextView = findViewById(R.id.txv)
         txv.text = "0"
+    }
+    fun happy(v: View){
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = (1..100).random().toString()
+
     }
 }
